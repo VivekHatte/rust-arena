ef2b669a3775f2a7e4e6144756dbdd2a038909cf
Vivek Hatte <vivek.hatte@gmail.com>
2026-09-17 20:47:41 -0700

    Got basic cli working with clap in rust

--- cli-unit-converter-project-2/src/main.rs
+++ cli-unit-converter-project-2/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(name = "converter")]
+#[command(version = "1.0")]
+#[command(about = "Converts from kilometers to miles and vice versa. Also does the same with farenheit and celcius", long_about = None)]
+struct Args {
+
+    #[arg(short, long, value_enum)]
+    unit: Unit,
+
+    #[arg(short, long)]
+    value: f32,
+
 }
+
+#[derive(ValueEnum, Clone, Copy, Debug, PartialEq, Eq)]
+enum Unit {
+    Kilometers,
+    Miles,
+    Celsius,
+    Fahrenheit,
+}
+fn main() {
+
+    let args = Args::parse();
+
+
+    println!("{}", args.value);
+
+    
+
+}
+
